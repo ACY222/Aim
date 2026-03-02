88858cb2db5eddf26a2e8fe685bd856c86b72f4a
ACY222 <838113915@qq.com>
2026-03-02 11:49:49 +0800

    improve error handling

--- aim.cpp
+++ aim.cpp
@@ -971,8 +971,13 @@ int main(int argc, char *argv[]) {
         }
         e.run();
     } catch (const std::system_error &e) {
-        std::cerr << "Fatal error: " << e.what() << "\r\n" << std::flush;
+        std::cerr << "System/Terminal Error: " << e.what() << "\r\n";
+    } catch (const std::exception &e) {
+        std::cerr << "Standard Library Exception: " << e.what() << "\r\n";
+    } catch (...) {
+        std::cerr << "Unknown Fatal Error occurred.\r\n";
     }
+
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
